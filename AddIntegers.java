7f0805f3043e82f79f59b538c90698e4608d0561
PatelNidhii <92067207+PatelNidhii@users.noreply.github.com>
2021-10-28 14:25:14 -0700

    Update AddIntegers.java

--- AddIntegers.java
+++ AddIntegers.java
@@ -3,8 +3,8 @@ public class AddIntegers {
     public static void main(String[] args) {
 
  Question2
-        int first = 45;
-        int second = 65;
+        int first = 15;
+        int second = 25;
  master
 
         System.out.println("Enter two numbers: " + first + " " + second);
